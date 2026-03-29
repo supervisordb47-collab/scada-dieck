b362dba29b2f2811d111593837e4d892aeeca982
supervisordb47-collab <supervisordb47@gmail.com>
2026-03-29 16:37:08 -0600

    Add seed data

---
+++ exacto/data.js
@@ -0,0 +1,34 @@
+window.DIECK_DATA = {
+  ui:{view:'order',mode:'paqueteria',date:'2026-03-29',github:{owner:'',repo:'',branch:'main',path:'data.json',token:''}},
+  github:{owner:'',repo:'',branch:'main',path:'data.json',token:''},
+  master:[
+    {codigo:'PT-106-002',descripcion:'ARROZ MANHATTAN ORO 1 LBS (25)',peso:25,unidad:'FARDO',area:'PAQUETERIA',entero_min:96,mignon_max:4,pulido_min:'22-23',tarima_div:80,bobina_x:5,bobina_y:25,lot_prefix:'AE',lot_base:3000},
+    {codigo:'PT-102-003',descripcion:'ARROZ MANHATTAN BLANCO 4 LBS (10)',peso:40,unidad:'FARDO',area:'PAQUETERIA',entero_min:96,mignon_max:4,pulido_min:'37-38',tarima_div:150,bobina_x:5,bobina_y:25,lot_prefix:'AB',lot_base:3000},
+    {codigo:'PT-107-002',descripcion:'ARROZ MR. DIECK BLANCO 350 GRS (25)',peso:19.2,unidad:'FARDO',area:'PAQUETERIA',entero_min:96,mignon_max:4,pulido_min:'37-38',tarima_div:80,bobina_x:4.347,bobina_y:25,lot_prefix:'AB',lot_base:3000},
+    {codigo:'PT-103-003',descripcion:'ARROZ MR. DIECK BLANCO 1.75 KGS (10)',peso:38.5,unidad:'FARDO',area:'PAQUETERIA',entero_min:96,mignon_max:4,pulido_min:'37-38',tarima_div:125,bobina_x:5,bobina_y:25,lot_prefix:'AB',lot_base:3000},
+    {codigo:'PT-104-011',descripcion:'ARROZ MANHATTAN BLANCO 1.5 KGS (10)',peso:33,unidad:'FARDO',area:'PAQUETERIA',entero_min:96,mignon_max:4,pulido_min:'37-38',tarima_div:80,bobina_x:5,bobina_y:25,lot_prefix:'AB',lot_base:3000},
+    {codigo:'PT-100-005',descripcion:'ARROZ MANHATTAN ORO QUINTAL 100 LBS',peso:100,unidad:'QQ',area:'M2',entero_min:96,mignon_max:4,pulido_min:'22-23',tarima_div:100,lot_prefix:'AE',lot_base:1225},
+    {codigo:'PT-100-007',descripcion:'ARROZ MR. DIECK BLANCO QUINTAL 100 LBS',peso:100,unidad:'QQ',area:'M1',entero_min:96,mignon_max:4,pulido_min:'37-38',tarima_div:100,lot_prefix:'AB',lot_base:1225},
+    {codigo:'PT-101-006',descripcion:'ARROZ MR. DIECK BLANCO SAQUITO 25 LBS',peso:25,unidad:'SAQ',area:'M1',entero_min:96,mignon_max:4,pulido_min:'37-38',tarima_div:100,lot_prefix:'AB',lot_base:1225}
+  ],
+  weeklyPlans:[
+    {fecha:'2026-03-23',semana:13,codigo:'PT-103-003',presentacion:'ARROZ MR. DIECK BLANCO 1.75 KGS (10)',area:'PAQUETERIA',programado:1000},
+    {fecha:'2026-03-23',semana:13,codigo:'PT-104-011',presentacion:'ARROZ MANHATTAN BLANCO 1.5 KGS (10)',area:'PAQUETERIA',programado:700},
+    {fecha:'2026-03-23',semana:13,codigo:'PT-102-003',presentacion:'ARROZ MANHATTAN BLANCO 4 LBS (10)',area:'PAQUETERIA',programado:360},
+    {fecha:'2026-03-23',semana:13,codigo:'PT-107-002',presentacion:'ARROZ MR. DIECK BLANCO 350 GRS (25)',area:'PAQUETERIA',programado:8000},
+    {fecha:'2026-03-23',semana:13,codigo:'PT-100-005',presentacion:'ARROZ MANHATTAN ORO QUINTAL 100 LBS',area:'M2',programado:700},
+    {fecha:'2026-03-23',semana:13,codigo:'PT-100-007',presentacion:'ARROZ MR. DIECK BLANCO QUINTAL 100 LBS',area:'M1',programado:1500}
+  ],
+  production:[
+    {fecha:'2026-03-23',dia:'LUNES',material:'PT-103-003',area:'PAQUETERIA',total:263},
+    {fecha:'2026-03-25',dia:'MIERCOLES',material:'PT-103-003',area:'PAQUETERIA',total:890},
+    {fecha:'2026-03-26',dia:'JUEVES',material:'PT-103-003',area:'PAQUETERIA',total:648},
+    {fecha:'2026-03-24',dia:'MARTES',material:'PT-107-002',area:'PAQUETERIA',total:1769},
+    {fecha:'2026-03-27',dia:'VIERNES',material:'PT-107-002',area:'PAQUETERIA',total:2431},
+    {fecha:'2026-03-28',dia:'SABADO',material:'PT-107-002',area:'PAQUETERIA',total:2431},
+    {fecha:'2026-03-23',dia:'LUNES',material:'PT-100-005',area:'M2',total:300},
+    {fecha:'2026-03-24',dia:'MARTES',material:'PT-100-005',area:'M2',total:700},
+    {fecha:'2026-03-25',dia:'MIERCOLES',material:'PT-100-005',area:'M2',total:120},
+    {fecha:'2026-03-26',dia:'JUEVES',material:'PT-100-005',area:'M2',total:768}
+  ]
+};
